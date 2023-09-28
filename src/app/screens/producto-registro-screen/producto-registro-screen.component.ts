6f55f6aeb0471e1935e8b93e66b026a126332fcb
Xanaks15 <josue.torresz@alumno.buap.mx>
2023-09-28 12:52:20 -0600

    ya furula

--- src/app/screens/producto-registro-screen/producto-registro-screen.component.ts
+++ src/app/screens/producto-registro-screen/producto-registro-screen.component.ts
@@ -1,8 +1,9 @@
-import { productosService } from './../../services/productos.service';
+import { ProductoService } from './../../services/productos.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-var $: any;
+import { ErrorsService } from 'src/app/services/tools/errors.service';
+declare var $: any;
 @Component({
   selector: 'app-producto-registro-screen',
   templateUrl: './producto-registro-screen.component.html',
@@ -13,13 +14,15 @@ export class ProductoRegistroScreenComponent implements OnInit {
   public editar:boolean = false;
   public producto: any = {};
   public errors:any ={};
+  
 constructor(
   private location: Location,
-  private ProductosService: productosService
+  private Router: Router,
+  private productosService: ProductoService,
 ) { }
 
 ngOnInit (): void {
-  this.producto = this.ProductosService.esquemaProducto;
+  this.producto = this.productosService.esquemaProducto;
     console.log("Producto: ", this.producto);
 }
 
@@ -27,14 +30,18 @@ public regresar() {
   this.location.back();
 }
 
+public home() {
+  this.Router.navigate([""]);
+}
 
 public registrar() { //No todas las rutas de acceso devuelven un valor
   this.errors = [];
 
-  this.errors = this.ProductosService.validarProducto(this.producto);
+  this.errors = this.productosService.validarProducto(this.producto);
   if (!$.isEmptyObject(this.errors)) {
     //Pasa la validación
     return false;
   }
+  alert("Todo chido vamos registrar producto");
 }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ValidatorService } from './tools/validator.service';
 import { ErrorsService } from './tools/errors.service';
+import { ValidatorService } from './tools/validator.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class productosService  {
+export class ProductoService {
 
   constructor(
     private validatorService: ValidatorService,
     private errorService: ErrorsService,
   ) { }
 
-  public esquemaProducto() {
-    return{
+  public esquemaProducto(){
+    return {
       'id': '',
       'nombre_producto': '',
       'precio': '',
       'departamento': '',
     }
-  }
+  } 
 
-  //Función para validar datos del usuario
-  public validarProducto(data: any) {
-    console.log("Validando Producto... ", data);
+  //Función para validar datos del producto
+  public validarProducto(data: any){
+    console.log("Validando producto... ", data);
     let error: any = [];
 
     if(!this.validatorService.required(data["id"])){
@@ -32,10 +33,12 @@ export class productosService  {
 
     if(!this.validatorService.required(data["nombre_producto"])){
       error["nombre_producto"] = this.errorService.required;
-    }else if(!this.validatorService.min(data["nombre_producto"], 1)){
-      error["nombre_producto"] = this.errorService.min(1);
+    }else if(!this.validatorService.min(data["nombre_producto"], 3)){
+      error["nombre_producto"] = this.errorService.min(3);
+      alert("La longitud de caracteres del nombre es menor a 3");
     }else if(!this.validatorService.max(data["nombre_producto"], 20)){
       error["nombre_producto"] = this.errorService.max(20);
+      alert("La longitud de caracteres del nombre es mayor a 20");
     }
 
     if(!this.validatorService.required(data["precio"])){
@@ -52,11 +55,13 @@ export class productosService  {
       error["departamento"] = this.errorService.required;
     }else if(!this.validatorService.min(data["departamento"], 3)){
       error["departamento"] = this.errorService.min(3);
+      alert("La longitud de caracteres del departamento es menor a 3");
     }else if(!this.validatorService.max(data["departamento"], 20)){
       error["departamento"] = this.errorService.max(20);
+      alert("La longitud de caracteres del departamento es mayor a 20");
     }
 
     return error;
-
+    
   }
-} 
+}
